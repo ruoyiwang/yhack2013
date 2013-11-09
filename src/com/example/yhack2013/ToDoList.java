363e46f80a97fe13b81bcafe9f7ee0e0261eceaa
David Guo <david.guo@uwaterloo.ca>
2013-11-09 03:43:04 -0500

    ToDoList

---
+++ src/com/example/yhack2013/ToDoList.java
@@ -0,0 +1,14 @@
+package com.example.yhack2013;
+
+public class ToDoList {
+	public ToDoList(int id_, int importance_, int duedate_, String description_) {
+		id = id_;
+		importance = importance_;
+		duedate = duedate_;
+		description_ = description;
+	}
+	public int id;
+	public int importance;
+	public int duedate;
+	public String description;
+}
