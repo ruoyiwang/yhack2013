2545501326d619fe362a9a308f85a6aabd8b06a0
unknown <wang.ruoyi.uw@gmail.com>
2013-11-09 02:35:23 -0500

    adding the add new item activity, no datapicker yet though

--- src/com/example/yhack2013/AddNewItem.java
+++ src/com/example/yhack2013/AddNewItem.java
@@ -1,8 +1,22 @@
 package com.example.yhack2013;
 
-import android.os.Bundle;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
 
 public class AddNewItem extends Activity {
 
@@ -10,6 +24,48 @@ public class AddNewItem extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_new_item);
+		
+		Button btnAddNewItem = (Button) findViewById(R.id.btnAddNew);
+		final EditText textItemName = (EditText) findViewById(R.id.textItemName);
+		final RatingBar rbImportance = (RatingBar) findViewById(R.id.ratingBar);
+		
+		
+		btnAddNewItem.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				int iImportance = (int) rbImportance.getRating();
+				String sItemName = textItemName.getText().toString();
+				// TODO get date
+				
+				// creating json
+				JSONObject top = new JSONObject();
+				JSONObject json = new JSONObject();
+				try {
+					json.put("id", "1");
+					json.put("ItemName", sItemName);
+					json.put("Importance", iImportance);
+					top.put("top", json);
+					// TODO add date
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				try {
+					String sOutput = top.toString();
+					String filename = "todo.json";
+					File file = new File(Environment.getExternalStorageDirectory().toString(), filename);
+					FileOutputStream fos;
+
+					fos = new FileOutputStream(file);
+					fos.write(sOutput.getBytes());
+					fos.flush();
+					fos.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 	@Override
